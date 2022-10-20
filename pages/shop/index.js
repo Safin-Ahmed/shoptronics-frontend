d5681c6ca710c3cd93a74d4b5ddcc9ea2c42973a
Naim uddin <naimuddin374@gmail.com>
2022-10-21 01:05:55 +0600

    Complete top pagination into shop page

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import TopPagination from "../../components/pagination/TopPagination";
 
 const Shop = () => {
   return (
     <div>
       <h1>This is Shop Page</h1>
+      <TopPagination />
     </div>
   );
 };
