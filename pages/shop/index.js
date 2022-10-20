a70cf6ec50ca39bc3c245bca6ced968d54d288d3
Safin Ahmed <safin.ahmed2000@gmail.com>
2022-10-21 00:16:18 +0600

    Added Material UI

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import FilterBar from "../../components/Shared/FilterBar";
 
 const Shop = () => {
   return (
     <div>
       <h1>This is Shop Page</h1>
+      <FilterBar />
     </div>
   );
 };
