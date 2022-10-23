b92c03a99acd66e155daa7f799ac76d8176183f6
Naiem Islam <naiem4288@gmail.com>
2022-10-23 20:01:55 +0600

    Create carousel banner and resolve the issue #24

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import CarouselBanner from '../components/carouselBanner/carouselBanner';
 import BestSellingSection from '../components/Section/BestSellingSection';
 import TrendingProductSection from '../components/Section/TrendingProductSection';
 
 export default function Home() {
   return (
     <div style={{ position: 'relative', zIndex: '1' }}>
+      <CarouselBanner/>
       <TrendingProductSection />
       <BestSellingSection />
     </div>
