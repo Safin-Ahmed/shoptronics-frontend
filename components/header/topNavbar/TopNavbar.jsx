63e9080da9065bf36a0fe810734da83220aeb317
Naiem Islam <naiem4288@gmail.com>
2022-10-23 07:18:15 +0600

    create top navbar for the header component and resolve the issue #7

--- components/header/topNavbar/TopNavbar.jsx
+++ components/header/topNavbar/TopNavbar.jsx
@@ -10,49 +10,7 @@ import InputField from '../../shared/InputField/InputField';
 import PrimaryLogo from '../../shared/primary-logo/PrimaryLogo';
 import HeaderAction from '../headerAction/HeaderAction';
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: '3px 0 0 3px',
-  borderColor: '#3577F0 !important',
-  padding: '5px 0',
-  alignItems: 'center',
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  width: '100%',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    flexGrow: 1,
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
 
 const TopNavbar = () => {
   return (
