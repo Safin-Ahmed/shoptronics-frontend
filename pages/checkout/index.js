9ff07c0a8fabdf1f3c804eb87f53ea9518b39620
Monir Uddin <monirkm74@gmail.com>
2022-10-26 12:59:10 +0600

    finished the checkout  page

--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -4,9 +4,36 @@ import Classes from './checkout.module.css';
 import TextFieldMUI from '@mui/material/TextField';
 import BreadcrumbsCom from "../../components/breadcrumbs/BreadcrumbsCom";
 import img from '../../public/static/product-4.png'
+import Product from '../../public/static/product-38.png'
+import Product1 from '../../public/static/product-1.jpg'
+import Product2 from '../../public/static/product-2.jpg'
+import Product4 from '../../public/static/product-4.png'
+import Product5 from '../../public/static/product-5.png'
 import Image from "next/image";
 
 const Checkout = () => {
+
+  const orderCarts = [
+   
+    {
+      productTitle : "Intel Core Two Duo RAM 4GB HDD 500GB",
+      productPrice : "$56.85",
+      productImage : Product2,
+      quantity: 2,
+    },
+    {
+      productTitle : "RAM 4GB HDD 500GB",
+      productPrice : "$100.85",
+      productImage : Product4,
+      quantity: 3,
+    },
+    {
+      productTitle : "RAM 4GB HDD 500GB",
+      productPrice : "$100.85",
+      productImage : Product5,
+      quantity: 1,
+    }
+   ]
   return (
     <div>
       <BreadcrumbsCom sx={{zIndex:"-9"}} breadcrumbs= "Cart" />
@@ -46,15 +73,25 @@ const Checkout = () => {
                 <div>
                   <Typography variant="h3">Your order</Typography>
                   <div className={Classes.checkoutRightProduct}>
-                    <Grid container  spacing={2}   className={Classes.checkoutRightProductitem}>
-                      <Grid item xs="3">
-                          <Image src={img} alt="Image Product"></Image>
-                      </Grid>
-                      <Grid item xs="9" sx={{alignSelf:"center"}} className={Classes.checkoutRightProductInfo}>
-                        <Typography variant="h5">Flexible WareLess Head Phone</Typography>
-                        <Typography> <span className={Classes.checkoutRightProductInfoPrice}></span>$234 X <span className={Classes.checkoutRightProductInfoQuntity}></span>2</Typography>
-                      </Grid>
-                    </Grid>
+
+                      {
+                        orderCarts.map((orderCart,index)=>{
+                          return(
+                            <Grid key={index} container  spacing={2}   className={Classes.checkoutRightProductitem}>
+                            <Grid item xs="3">
+                                <Image src={orderCart.productImage} alt="Image Product"></Image>
+                            </Grid>
+                            <Grid item xs="9" sx={{alignSelf:"center"}} className={Classes.checkoutRightProductInfo}>
+                              <Typography variant="h5">{orderCart.productTitle}</Typography>
+                              <Typography> <span className={Classes.checkoutRightProductInfoPrice}></span>{orderCart.productPrice} X <span className={Classes.checkoutRightProductInfoQuntity}></span>{orderCart.quantity}</Typography>
+                            </Grid>
+                          </Grid>
+                          );
+                        })
+                      }
+
+                   
+
                   </div>
                   <ul>
                     <li><span>Subtotal (2 items)</span> <span>$698</span></li>
@@ -83,7 +120,7 @@ const Checkout = () => {
                   </ul>
                 </div>
                 <div>
-                  <Typography variant="h3">Shipping</Typography>
+                  <Typography variant="h3">Payment</Typography>
                   <div className={Classes.checkoutRightinfo}>
                   <FormControl>
                  
@@ -96,12 +133,8 @@ const Checkout = () => {
                       <FormControlLabel value="male" control={<Radio />} label="Bkash" className={Classes.checkoutRightRadio}/>
                     </RadioGroup>
                   </FormControl>
-                  </div>
-                  
-                </div>
-                
-
-                
+                  </div>   
+                </div> 
                 <div className={Classes.cartRightbtnn}>
                 <Button className={Classes.cartRightbtn} variant="contained">Place Order</Button>
                 </div>
