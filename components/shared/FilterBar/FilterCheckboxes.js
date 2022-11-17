87ae127867185abdad6a23d6beb093d93b058c42
Safin Ahmed <safin.ahmed2000@gmail.com>
2022-11-17 17:29:12 +0600

    Fixed cases

---
+++ components/shared/FilterBar/FilterCheckboxes.js
@@ -0,0 +1,62 @@
+import { useQuery } from "@apollo/client";
+import React from "react";
+import {
+  brandsQuery,
+  getAllAttributes,
+  getAllAttributesTerms,
+} from "../../../lib/queries";
+import { formatString } from "../../../utils/string";
+import { buildAttributesCheckboxOptions } from "../../../utils/variations";
+import FilterCheckbox from "./FilterCheckbox";
+
+const FilterCheckboxes = () => {
+  const { data, loading, error } = useQuery(brandsQuery);
+  const brandOptions = data?.brands?.data?.reduce((acc, cur) => {
+    acc.push(cur.attributes.name);
+    return acc;
+  }, []);
+
+  const { data: allAttributesData, loading: attributesLoading } =
+    useQuery(getAllAttributes);
+  const { data: allAttributesTermData, loading: attributesTermLoading } =
+    useQuery(getAllAttributesTerms);
+
+  if (attributesLoading || attributesTermLoading) {
+    return;
+  }
+  const attributes = buildAttributesCheckboxOptions(
+    allAttributesData,
+    allAttributesTermData
+  );
+
+  return (
+    <>
+      <FilterCheckbox
+        title="Availability"
+        options={["In Stock", "Out of Stock"]}
+        type="filter_availability"
+      />
+      <FilterCheckbox
+        title="Brand"
+        options={brandOptions}
+        type="filter_brand"
+      />
+
+      {attributes?.map((item) => (
+        <FilterCheckbox
+          key={`${item.name} - ${item.id}`}
+          title={item.name}
+          options={item.options}
+          type={`attribute_${formatString(item.name)}`}
+        />
+      ))}
+      <FilterCheckbox
+        title={"Rating"}
+        options={[5, 4, 3, 2, 1]}
+        type="filter_rating"
+      />
+    </>
+  );
+};
+
+export default FilterCheckboxes;
---
+++ components/shared/FilterBar/FilterCollapse/FilterCollapseList.js
@@ -0,0 +1,96 @@
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { Button, Typography } from "@mui/material";
+
+import useFilterCollapseList from "../../../../hooks/useFilterCollapseList";
+import { formatString } from "../../../../utils/string";
+import { useEffect } from "react";
+
+export default function FilterCollapseList({
+  options,
+  searchTerm,
+  setSearchTerm,
+  setIsSelected,
+}) {
+  const { finalOptionsList, handleViewAll, handleQuery, queries } =
+    useFilterCollapseList(
+      options,
+      searchTerm,
+      setSearchTerm,
+      "filter_category"
+    );
+
+  useEffect(() => {
+    const isSelected = finalOptionsList
+      .map((item) => queries?.includes(item.toLowerCase()))
+      .includes(true);
+
+    if (isSelected) {
+      setIsSelected(true);
+    } else {
+      setIsSelected(false);
+    }
+  }, [queries, finalOptionsList, setIsSelected]);
+
+  return (
+    <List sx={{ width: "95%", margin: "auto", bgcolor: "background.paper" }}>
+      {options.length > 4 && (
+        <ListItem
+          sx={{ py: 0, my: 0 }}
+          disableGutters
+          secondaryAction={
+            <Typography sx={{ fontSize: "14px", fontFamily: "Rubik" }}>
+              30
+            </Typography>
+          }
+        >
+          <ListItemText>
+            <Button
+              variant="text"
+              onClick={handleViewAll}
+              sx={{
+                color: "rgba(0, 0, 0, 0.8)",
+                p: 0,
+                justifyContent: "flex-start",
+                fontSize: "14px",
+                fontFamily: "Rubik",
+                "&:hover": { background: "none", textDecoration: "underline" },
+              }}
+            >{`View All`}</Button>
+          </ListItemText>
+        </ListItem>
+      )}
+      {finalOptionsList.map((value) => (
+        <ListItem
+          sx={{ py: 0, my: 0 }}
+          key={value}
+          disableGutters
+          secondaryAction={
+            <Typography sx={{ fontSize: "14px", fontFamily: "Rubik" }}>
+              30
+            </Typography>
+          }
+        >
+          <ListItemText>
+            <Button
+              variant="text"
+              onClick={() => handleQuery({ param: "category", id: value })}
+              sx={{
+                color: "rgba(0, 0, 0, 0.8)",
+                p: 0,
+                justifyContent: "flex-start",
+                fontSize: "14px",
+                fontFamily: "Rubik",
+                textDecoration: queries?.includes(formatString(value))
+                  ? "underline"
+                  : "",
+                "&:hover": { background: "none", textDecoration: "underline" },
+              }}
+            >{`${value}`}</Button>
+          </ListItemText>
+        </ListItem>
+      ))}
+    </List>
+  );
+}
